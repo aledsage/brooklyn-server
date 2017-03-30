b29734389d2d9c49753f35b625c0ffb766424105
Aled Sage <aled.sage@gmail.com>
2017-03-30 12:37:36 +0100

    CreateUserPolicy resets password periodically [WIP]

--- locations/jclouds/src/main/java/org/apache/brooklyn/policy/jclouds/os/CreateUserPolicy.java
+++ locations/jclouds/src/main/java/org/apache/brooklyn/policy/jclouds/os/CreateUserPolicy.java
@@ -18,25 +18,37 @@
  */
 package org.apache.brooklyn.policy.jclouds.os;
 
-import java.util.List;
+import static org.apache.brooklyn.util.ssh.BashCommands.sbinPath;
 
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.brooklyn.api.effector.Effector;
 import org.apache.brooklyn.api.entity.Entity;
 import org.apache.brooklyn.api.entity.EntityLocal;
 import org.apache.brooklyn.api.location.Location;
+import org.apache.brooklyn.api.mgmt.Task;
 import org.apache.brooklyn.api.sensor.AttributeSensor;
 import org.apache.brooklyn.api.sensor.SensorEvent;
 import org.apache.brooklyn.api.sensor.SensorEventListener;
 import org.apache.brooklyn.config.ConfigKey;
 import org.apache.brooklyn.core.config.ConfigKeys;
+import org.apache.brooklyn.core.effector.EffectorBody;
+import org.apache.brooklyn.core.effector.Effectors;
 import org.apache.brooklyn.core.entity.AbstractEntity;
 import org.apache.brooklyn.core.entity.EntityInternal;
 import org.apache.brooklyn.core.policy.AbstractPolicy;
 import org.apache.brooklyn.core.sensor.Sensors;
 import org.apache.brooklyn.location.ssh.SshMachineLocation;
+import org.apache.brooklyn.util.core.config.ConfigBag;
 import org.apache.brooklyn.util.core.flags.SetFromFlag;
 import org.apache.brooklyn.util.core.internal.ssh.SshTool;
-import static org.apache.brooklyn.util.ssh.BashCommands.sbinPath;
+import org.apache.brooklyn.util.core.task.BasicTask;
+import org.apache.brooklyn.util.core.task.ScheduledTask;
+import org.apache.brooklyn.util.exceptions.Exceptions;
 import org.apache.brooklyn.util.text.Identifiers;
+import org.apache.brooklyn.util.time.Duration;
 import org.jclouds.compute.config.AdminAccessConfiguration;
 import org.jclouds.scriptbuilder.functions.InitAdminAccess;
 import org.jclouds.scriptbuilder.statements.login.AdminAccess;
@@ -73,6 +85,23 @@ public class CreateUserPolicy extends AbstractPolicy implements SensorEventListe
     
     // TODO Could make the password explicitly configurable, or auto-generate if not set?
     
+    // TODO Scheduling of password-reset is illustrative code only; it is untested and not
+    // cleaned up.
+    // 
+    // The idea is: when the policy is created, e.g. to change the password every 21 days, it 
+    // schedules a task to execute in 21 days. When that task executes, it changes the password
+    // and then schedules itself again for another 21 days time.
+    // If Brooklyn is restarted in that time, our task will be lost. On rebind, it therefore
+    // re-schedules the task. It stores the "next-time" as an internal config value so it knows
+    // when to reschedule for.
+    //
+    // The implementation relies on config().set(CHANGE_PASSWORD_NEXT_TIME) causing 
+    // doReconfigureConfig() to be called.
+    // 
+    // The ability to reconfigure the changePasswordPeriod on-the-fly makes things more complicated
+    // (including for testing - maybe we shouldn't support that yet?).
+    // It re-computes when the password should expire, and resets it.
+    
     private static final Logger LOG = LoggerFactory.getLogger(CreateUserPolicy.class);
 
     @SetFromFlag("user")
@@ -94,10 +123,35 @@ public class CreateUserPolicy extends AbstractPolicy implements SensorEventListe
             "Whether to reset the password used for user login",
             false);
 
+    public static final ConfigKey<Duration> CHANGE_PASSWORD_PERIOD = ConfigKeys.newConfigKey(
+            Duration.class,
+            "createuser.vm.user.changePasswordPeriod",
+            "The length of time between automatically changing the password (or null to disable)",
+            null);
+
+    public static final ConfigKey<Date> CHANGE_PASSWORD_NEXT_TIME = ConfigKeys.newConfigKey(
+            Date.class,
+            "createuser.vm.user.changePasswordNextTime",
+            "the date when the password will next be reset (or null); internal - set automatically based on changePasswordPeriod",
+            null);
+
+    private ScheduledTask changePasswordScheduledTask;
+    
     @Override
     public void setEntity(EntityLocal entity) {
         super.setEntity(entity);
         subscriptions().subscribe(entity, AbstractEntity.LOCATION_ADDED, this);
+        
+        // Schedule the password-change, if configured to do so.
+        //
+        // This code is called when first creating the policy, and also on rebind.
+        // For the latter, we must schedule our task again, to execute at the "next time".
+        initChangePasswordSchedule();
+        
+        if (!isRebinding()) {
+            // Add the updateUserPassword effector to the entity
+            addUpdateUserPasswordEffector();
+        }
     }
 
     @Override
@@ -179,5 +233,121 @@ public class CreateUserPolicy extends AbstractPolicy implements SensorEventListe
         }
         
         entity.sensors().set(VM_USER_CREDENTIALS, creds);
+    }
+    
+    protected void changePasswordNow() {
+        // TODO Execute over ssh jclouds `ReplaceShadowPasswordEntry`
+        // Also call entity.sensors().set(VM_USER_CREDENTIALS, ...)
+    }
+
+    protected void updatePasswordSensor(String newPassword) {
+        // TODO Call entity.sensors().set(VM_USER_CREDENTIALS, ...)
+    }
+    
+    @Override
+    protected <T> void doReconfigureConfig(ConfigKey<T> key, T val) {
+        if (key.equals(CHANGE_PASSWORD_NEXT_TIME)) {
+            if (val == null) {
+                cancelChangePasswordScheduledTask();
+            } else {
+                scheduleChangePassword((Date)val);
+            }
+        } else if (key.equals(CHANGE_PASSWORD_PERIOD)) {
+            if (val == null) {
+                config().set(CHANGE_PASSWORD_NEXT_TIME, null);
+            } else {
+                Date nextTime = config().get(CHANGE_PASSWORD_NEXT_TIME);
+                if (nextTime == null) {
+                    Date newNextTime = new Date(System.currentTimeMillis() + ((Duration)val).toMilliseconds());
+                    config().set(CHANGE_PASSWORD_NEXT_TIME, newNextTime);
+                } else {
+                    Duration oldPeriod = config().get(CHANGE_PASSWORD_PERIOD);
+                    long diffPeriod = ((Duration)val).subtract(oldPeriod).toMilliseconds();
+                    long newNextTimestamp = nextTime.getTime() + diffPeriod;
+                    Date newNextTime = (newNextTimestamp < System.currentTimeMillis()) ? new Date() : new Date(newNextTimestamp);
+                    config().set(CHANGE_PASSWORD_NEXT_TIME, newNextTime);
+                }
+            }
+        } else {
+            super.doReconfigureConfig(key, val);
+        }
+    }
+    
+    private void cancelChangePasswordScheduledTask() {
+        if (changePasswordScheduledTask != null) {
+            changePasswordScheduledTask.cancel();
+            changePasswordScheduledTask = null;
+        }
+    }
+    
+    private void scheduleChangePassword(Date when) {
+        cancelChangePasswordScheduledTask();
+        
+        long delayMillis = when.getTime() - System.currentTimeMillis();
+        Duration delay = (delayMillis <= 0) ? Duration.ZERO : Duration.millis(delayMillis);
+        
+        Callable<Task<?>> taskFactory = new Callable<Task<?>>() {
+            @Override public Task<?> call() {
+                return new BasicTask<Void>(new Runnable() {
+                    @Override public void run() {
+                        try {
+                            changePasswordNow();
+                            reschedule(config().get(CHANGE_PASSWORD_PERIOD));
+                        } catch (Exception e) {
+                            Exceptions.propagateIfFatal(e);
+                            Duration period = config().get(CHANGE_PASSWORD_PERIOD);
+                            if (period == null) {
+                                LOG.warn("Problem reseting password for "+getEntity()+"; will not retry because "
+                                        + CHANGE_PASSWORD_PERIOD.getName()+" is now null", e);
+                            } else {
+                                Duration delay = Duration.FIVE_MINUTES;
+                                LOG.warn("Problem reseting password for "+getEntity()+"; will retry in "+delay, e);
+                                reschedule(delay);
+                            }
+                        }
+                    }
+                    private void reschedule(Duration delay) {
+                        if (delay != null) {
+                            Date changePasswordNextTime = new Date(System.currentTimeMillis() + delay.toMilliseconds());
+                            config().set(CHANGE_PASSWORD_NEXT_TIME, changePasswordNextTime);
+                        } else {
+                            config().set(CHANGE_PASSWORD_NEXT_TIME, null);
+                        }
+                    }});
+            }};
+
+        LOG.info("Scheduling change-password for {} in {}", getEntity(), delay);
+        ((EntityInternal)entity).getExecutionContext().submit(new ScheduledTask(taskFactory).delay(delay));
+        changePasswordScheduledTask = new ScheduledTask(taskFactory).delay(delay);
+    }
+
+    private void initChangePasswordSchedule() {
+        Duration changePasswordPeriod = config().get(CHANGE_PASSWORD_PERIOD);
+        Date changePasswordNextTime = config().get(CHANGE_PASSWORD_NEXT_TIME);
+        if (changePasswordPeriod != null) {
+            if (changePasswordNextTime == null) {
+                changePasswordNextTime = new Date(System.currentTimeMillis() + changePasswordPeriod.toMilliseconds());
+                config().set(CHANGE_PASSWORD_NEXT_TIME, changePasswordNextTime); // this calls doReconfigureConfig
+            } else {
+                scheduleChangePassword(changePasswordNextTime);
+            }
+        }
+    }
+    
+    private void addUpdateUserPasswordEffector() {
+        final ConfigKey<String> newPasswordParam = ConfigKeys.newStringConfigKey("newPassword");
+        Effector<Void> updateUserPasswordEffector = Effectors.effector(Void.class, "updateUserPassword")
+                .parameter(newPasswordParam)
+                .impl(new EffectorBody<Void>() {
+                    @Override
+                    public Void call(ConfigBag parameters) {
+                        String newPassword = parameters.get(newPasswordParam);
+                        // TODO null-checks, etc
+                        updatePasswordSensor(newPassword);
+                        return null;
+                    }})
+                .build();
+
+        ((EntityInternal)entity).getMutableEntityType().addEffector(updateUserPasswordEffector);
     }
 }
